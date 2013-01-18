9113d2320cb842c76f0a762e71a7c9996d1f1d81
3876780 <3876780@21e0bece-7d3f-4eef-9ee9-5dbb865cd082>
2013-01-18 15:37:53 +0000

    Added While function for one pattern (Move).

--- ant-gen/AntMap.hs
+++ ant-gen/AntMap.hs
@@ -27,6 +27,55 @@ aTurn d = do
     idx <- supply
     return $ AntStrategy' (M.fromList [(idx, Turn d idx)]) idx idx
 
+aSense :: Direction -> Condition -> AntStrategy -> AntStrategy
+aSense d c exception = do
+    idx <- supply
+    e <- exception
+    return $ AntStrategy' (M.insert idx (Sense d idx (initial e) c) (instructions e)) 
+                          idx 
+                          idx
+
+{-
+-- No infinite loops, for now
+aLoop :: AntStrategy -> AntStrategy
+aLoop as = do
+    as' <- as
+    let asFinalInstr = fromJust $ M.lookup (final as') (instructions as') 
+        newInstrs    = M.insert (final as') 
+                                (replaceDefaultState (initial as') asFinalInstr) 
+                                (instructions as')
+    return $ AntStrategy' newInstrs (initial as') (final as')
+-}
+
+data AntTest = TryForward
+             | TryPickup
+             | TrySense Direction Condition
+
+-- TODO: finish all patterns
+aWhile :: AntTest -> AntStrategy -> AntStrategy -> AntStrategy
+aWhile TryForward s1 s2 = do
+    s1' <- s1
+    s2' <- s2
+    idx <- supply
+    let testInstr = Move t f
+        (t,f)     = (initial s1',initial s2')
+        s1''      = replaceFinal idx s1'
+    return $ AntStrategy' (M.insert idx testInstr ((instructions s1'') `M.union` 
+                                                   (instructions s2')))
+                          idx
+                          (final s2')
+aWhile _ _ _ = undefined
+
+replaceFinal :: AntState -> AntStrategy' -> AntStrategy'
+replaceFinal idx as = 
+    let asFinalInstr = fromJust $ M.lookup (final as) (instructions as) 
+        newInstrs    = M.insert (final as) 
+                                (replaceDefaultState idx asFinalInstr) 
+                                (instructions as)
+     in AntStrategy' newInstrs (initial as) (final as) 
+    
+
+runAnt p = fst $ runSupply p [AntState 0..]    
 
 replaceDefaultState :: AntState -> AntInstruction -> AntInstruction
 replaceDefaultState ns (Sense d _ s c) = (Sense d ns s c)

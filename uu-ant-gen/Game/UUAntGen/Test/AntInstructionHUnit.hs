9ee24067b2b3e7283404e105966b9fc988f83bc7
3860418 <3860418@21e0bece-7d3f-4eef-9ee9-5dbb865cd082>
2013-01-22 10:53:50 +0000

    fixed the Hunit tests and the cabal file

--- uu-ant-gen/Game/UUAntGen/Test/AntInstructionHUnit.hs
+++ uu-ant-gen/Game/UUAntGen/Test/AntInstructionHUnit.hs
@@ -1,13 +1,32 @@
 module Game.UUAntGen.Test.AntInstructionHUnit where
 
-import Test.HUnit ((~?=), test, (~:))
-import qualified Data.Map as M
-import Game.UUAntGen.AntMap (aDrop, getAntStrategy, AntStrategy'(..))
-import Game.UUAntGen.AntInstruction
+import qualified Data.HashMap.Lazy            as M
+import           Game.UUAntGen.AntInstruction
+import           Game.UUAntGen.AntMap         (AntStrategy' (..), aDrop, aTurnL, getAntStrategy)
+import           Test.HUnit                   (Test, test, (~:), (~?=))
 
 
+testAntInstruction :: Test
+testAntInstruction = "testAntInstruction" ~: test $
+    [ testDrop
+    , testTurnL ]
+
+
+s0, s1, s2, s3, s4, s5 :: AntState
+s0 = AntState 0
+s1 = AntState 1
+s2 = AntState 2
+s3 = AntState 3
+s4 = AntState 4
+s5 = AntState 5
+
+
+testDrop :: Test
 testDrop = "testDrop" ~:  getAntStrategy aDrop ~?= expected
-    where
-        z        = AntState 0
-        expected = AntStrategy' (M.fromList [(z, Drop z)]) z z
+    where expected = AntStrategy' (M.fromList [(s0, Drop s0)]) s0 s0
+
+
+testTurnL :: Test
+testTurnL = "testTurnL" ~:  getAntStrategy aTurnL ~?= expected
+    where expected = AntStrategy' (M.fromList [(s0, Turn L s0)]) s0 s0
 

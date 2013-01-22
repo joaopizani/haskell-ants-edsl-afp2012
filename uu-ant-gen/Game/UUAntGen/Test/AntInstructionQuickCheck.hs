5a78b46f542cba19e2c2e0aa0b79637e77b5ef89
3876780 <3876780@21e0bece-7d3f-4eef-9ee9-5dbb865cd082>
2013-01-22 18:05:49 +0000

    Implemented QuickCheck property lastRefsItself and seems to be working, although it needs some fixes... Followed the approach described in "Testing Monadic Code with QuickCheck" paper.

--- uu-ant-gen/Game/UUAntGen/Test/AntInstructionQuickCheck.hs
+++ uu-ant-gen/Game/UUAntGen/Test/AntInstructionQuickCheck.hs
@@ -1,12 +1,71 @@
 module Game.UUAntGen.Test.AntInstructionQuickCheck where
 
-import Test.QuickCheck   (Gen(..),arbitrary,Property,oneof,forAll)
-import Control.Monad     (liftM)
-import Data.HashMap.Lazy ((!))
+import Test.QuickCheck.Monadic
+import Test.QuickCheck   (Gen(..),Arbitrary(..),arbitrary,elements,Property
+                         ,oneof,forAll,listOf)
+import Control.Monad     (liftM,liftM2)
+import Data.Map          ((!))
+
+import Control.Monad.Supply
 
 import Game.UUAntGen.Test.AntInstructionArbitrary
 import Game.UUAntGen.AntMap
 import Game.UUAntGen.AntInstruction
+
+--PropertyM (Supply AntState) AntStrategy' 
+
+data Action  = Single Command
+    deriving Show
+data Command = CDrop
+             | CTurn Dexterity 
+    deriving Show
+
+
+semantics :: AntStrategy -> [Action] -> AntStrategy
+semantics as []     = as
+semantics as (a:tl) =
+    case a of
+      Single CDrop     -> as >>- (semantics aDrop tl)
+      Single (CTurn d) -> as >>- (semantics (aTurn d) tl)
+
+perform :: [Action] -> AntStrategy
+perform []     = aDrop -- this should be working on non-empty lists...
+perform (a:tl) = 
+    case a of
+      Single CDrop     -> semantics aDrop tl
+      Single (CTurn d) -> semantics (aTurn d) tl
+
+actions :: Gen [Action]
+actions = liftM2 (:) arbitrary (listOf arbitrary)
+
+instance Arbitrary Action where
+    arbitrary = liftM Single arbitrary
+
+instance Arbitrary Command where
+    arbitrary = elements [CDrop,CTurn L,CTurn R]
+
+supplyProp :: PropertyM (Supply AntState) a -> Property
+supplyProp p = monadic f p
+    where f :: Supply AntState Property -> Property
+          f s = fst $ runSupply s [AntState 0..]
+
+finalRefsItselfProp :: Property
+finalRefsItselfProp = supplyProp finalRefsItselfPropM
+
+finalRefsItselfPropM :: PropertyM (Supply AntState) ()
+finalRefsItselfPropM = forAllM actions $ \as -> do
+    b <- run (perform as)
+    assert (lastRefsItself b)
+    return ()
+
+singleInstructionProp :: Property
+singleInstructionProp = supplyProp singleInstructionPropM 
+
+singleInstructionPropM :: PropertyM (Supply AntState) () 
+singleInstructionPropM = do
+    b <- run aDrop -- FIXME: add more actions
+    assert (refsItself b)
+
 
 singleInstructionGen :: Gen AntStrategy'
 singleInstructionGen = oneof [ liftM aMark' arbitrary 
@@ -15,6 +74,9 @@ singleInstructionGen = oneof [ liftM aMark' arbitrary
                              , return aTurnL' 
                              , return aTurnR'
                              ]
+
+genTest :: Gen (AntStrategy)
+genTest = return aDrop
 
 aMark' :: Pheromone -> AntStrategy'
 aMark' p = aMkSingletonStrategy' (Mark p) (AntState 0)
@@ -36,11 +98,18 @@ aTurnR' = aTurn' R
 
 singleInstructionFinal :: Property
 singleInstructionFinal = forAll singleInstructionGen refsItself
-    where refsItself :: AntStrategy' -> Bool
-          refsItself as = let ins  = instructions as
-                              init = initial as
-                           in init == final as &&
-                              getDefaultState (ins ! init) == init
+
+refsItself :: AntStrategy' -> Bool
+refsItself as = let ins  = instructions as
+                    init = initial as
+                    fin  = final as
+                 in init == fin &&
+                    getDefaultState (ins ! init) == init
+
+lastRefsItself :: AntStrategy' -> Bool
+lastRefsItself as = let ins  = instructions as
+                        fin  = final as
+                     in getDefaultState (ins ! fin) == fin
 
 getDefaultState (Sense d _ s c) = s 
 getDefaultState (Mark _ s)      = s

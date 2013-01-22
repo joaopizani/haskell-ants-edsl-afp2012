1a7b0837c822675b5f621adb392fb21da64e91d3
3876780 <3876780@21e0bece-7d3f-4eef-9ee9-5dbb865cd082>
2013-01-22 10:14:58 +0000

    Didn't manage go get an acceptable implementation for AntReify...

--- uu-ant-gen/Game/UUAntGen/AntReify.hs
+++ uu-ant-gen/Game/UUAntGen/AntReify.hs
@@ -2,7 +2,9 @@ module Game.UUAntGen.AntReify where
 
 import GHC.Vacuum
 import Data.IntMap as M
+import GHC.Vacuum.GraphViz
 
+main = vacuumToPng "foo" exWhile >> return ()
 
 program :: AntInstruction
 program = Drop (Flip 25 program program)
@@ -17,10 +19,50 @@ inspect hnode = (nodePtrs hnode, f (nodeInfo hnode))
                      lits ty slen code) | con == "I#" = show $ head $ nodeLits hnode
                                         | otherwise   = con
           f _                                         = "fun"
- 
+
+
+(>>-) :: AntInstruction -> AntInstruction -> AntInstruction
+Hole              >>- a  = a
+(Sense d a1 a2 c) >>- a3 = Sense d a1 (a2 >>- a3) c
+(Mark p a1)       >>- a2 = Mark p (a1 >>- a2)
+(UnMark p a1)     >>- a2 = UnMark p (a1 >>- a2)
+(PickUp a1 a2)    >>- a3 = PickUp a1 (a2 >>- a3)
+(Drop a1)         >>- a2 = Drop (a1 >>- a2)
+(Turn d a1)       >>- a2 = Turn d (a1 >>- a2)
+(Move a1 a2)      >>- a3 = Move a1 (a2 >>- a1)
+
+-- | Creates a while-style loop
+-- Condition -> Body -> Continuation
+aWhile :: AntTest -> AntInstruction -> AntInstruction -> AntInstruction
+aWhile TryForward          = aMkWhile Move 
+aWhile TryPickup           = aMkWhile PickUp 
+aWhile (TrySense d c)      = aMkWhile (\t f -> Sense d t f c)
+aWhile (TryRandomEqZero i) = aMkWhile (flip (Flip i))
+
+aMkWhile :: (AntInstruction -> AntInstruction -> AntInstruction)
+         -> AntInstruction -> AntInstruction -> AntInstruction 
+aMkWhile g t f = let t'    = t >>- while
+                     while = g t' f 
+                  in while 
+
+while :: AntInstruction -> AntInstruction -> AntInstruction
+while body rest = let while = Move (body >>- while) rest
+                   in while
+
+aDrop = Drop Hole
+aTurnL = Turn L Hole
+
+exWhile = aWhile TryForward (aWhile TryPickup (Drop Hole) (Mark P0 Hole))
+                            (Turn L Hole)
+
+data AntTest
+    = TryForward
+    | TryPickup
+    | TrySense Direction Condition
+    | TryRandomEqZero Int
 
 data AntInstruction
-    = Sense Direction AntInstruction Condition
+    = Sense Direction AntInstruction AntInstruction Condition
     | Mark Pheromone AntInstruction
     | UnMark Pheromone AntInstruction
     | PickUp AntInstruction AntInstruction
@@ -28,6 +70,7 @@ data AntInstruction
     | Turn Dexterity AntInstruction
     | Move AntInstruction AntInstruction
     | Flip Int AntInstruction AntInstruction
+    | Hole
     deriving Show
 
 data Pheromone = P0 | P1 | P2 | P3 | P4 | P5

3d7f68a64fb5fbd713e77fe8806666087f3240a3
3132315 <3132315@21e0bece-7d3f-4eef-9ee9-5dbb865cd082>
2013-01-24 22:08:28 +0000

    Main for printing the file (argument 0)

--- uu-ant-gen/UUAntGen.hs
+++ uu-ant-gen/UUAntGen.hs
@@ -1,4 +1,14 @@
 module Main where
 
-main = putStrLn "Hello, world!"
+import Game.UUAntGen.AntBasic;
+import Game.UUAntGen.AntImperative;
+import Game.UUAntGen.AntInstruction;
+import Game.UUAntGen.AntMoves;
+import Game.UUAntGen.AntTransformation;
+import System.Environment;
 
+main = do 
+    [path] <- getArgs
+    writeFile path $ printInstructionMap $ fromKeysToLineNumbers $ ghostBuster $ runAntStrategy $ strategy
+
+strategy = goForwardNSteps 10
